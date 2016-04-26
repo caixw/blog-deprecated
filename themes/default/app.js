43ec73c8a25ae9e36bc666b8f56e3ae172bf7a72
caixw <xnotepad@gmail.com>
2016-04-26 18:13:35 +0800

    添加语法高亮的部分插件 版本号改为官方的1.4.1而不是bootcn上的9000.0.1，该版本部分功能不全； 添加autoloader插件，用于自动匹配加载相应语言的语法解析文件； 添加line-numbers插件，用于显示行号功能。

--- themes/default/app.js
+++ themes/default/app.js
@@ -1,29 +1,35 @@
 "use strict";
 
-// 根据与页面顶部的距离，控制是否显示top按钮。
-$(window).on('scroll', function(){
-    var button = $('#top');
-    if($(document).scrollTop() > 30){
-        button.fadeIn();
-    }else{
-        button.fadeOut();
-    }
-}).trigger('scroll'); // end $(window).onscroll
+$(document).ready(function(){
+    // 语法高亮
+    Prism.plugins.autoloader.languages_path='//cdn.bootcss.com/prism/1.4.1/components/';
 
-// 滚动到顶部
-$('#top').on('click', function(){
-    var times = 20;
-    var height = $('#top').offset().top;
-    var offset = height / times;
-
-    var tick = window.setInterval(function(){
-        height -= offset;
-        window.scrollTo(0, height);
-
-        if (height <= 0){
-            window.clearInterval(tick);
+    // 根据与页面顶部的距离，控制是否显示top按钮。
+    $(window).on('scroll', function(){
+        var button = $('#top');
+        if($(document).scrollTop() > 30){
+            button.fadeIn();
+        }else{
+            button.fadeOut();
         }
-    }, 10);
+    }).trigger('scroll'); // end $(window).onscroll
 
-    return false;
-});
+    // 滚动到顶部
+    $('#top').on('click', function(){
+        var times = 20;
+        var height = $('#top').offset().top;
+        var offset = height / times;
+
+        var tick = window.setInterval(function(){
+            height -= offset;
+            window.scrollTo(0, height);
+
+            if (height <= 0){
+                window.clearInterval(tick);
+            }
+        }, 10);
+
+        return false;
+    });
+
+}); // end $(document).ready()
