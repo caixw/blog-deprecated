c7dc817e6745c8813d9ef3351d78475d62afec6f
caixw <xnotepad@gmail.com>
2017-08-22 19:06:56 +0800

    去掉对 jQuery 的依赖

--- themes/default/app.js
+++ themes/default/app.js
@@ -1,24 +1,36 @@
 "use strict";
 
-$(function(){
+window.document.addEventListener('DOMContentLoaded', ()=>{
     // 语法高亮
     Prism.plugins.autoloader.languages_path='//cdn.bootcss.com/prism/1.6.0/components/';
     Prism.highlightAll(false);
 
+    initTop()
+})
+
+// 初始化所有与 goto top 相关的内容
+function initTop () {
+    const top = window.document.getElementById('top')
+
+    const scrollTop = function() {
+        return document.body.scrollTop + document.documentElement.scrollTop
+    }
+
     // 根据与页面顶部的距离，控制是否显示 top 按钮。
-    $(window).on('scroll', function(){
-        const button = $('#top');
-        if($(document).scrollTop() > 30){
-            button.fadeIn();
-        }else{
-            button.fadeOut();
+    const showTopButton = function() {
+        const dsp = scrollTop() > 30 ? 'block' : 'none'
+        if (dsp !== top.style.display) {
+            top.style.display = dsp
         }
-    }).trigger('scroll'); // end $(window).onscroll
+    }
+
+    showTopButton()
+    window.addEventListener('scroll', ()=>{showTopButton()})
 
     // 滚动到顶部
-    $('#top').on('click', function(){
+    top.addEventListener('click', ()=>{
         const times = 20;
-        let height = $('#top').offset().top;
+        let height = scrollTop()
         const offset = height / times;
 
         const tick = window.setInterval(function(){
@@ -32,5 +44,4 @@ $(function(){
 
         return false;
     });
-
-}); // end $.ready()
+}
