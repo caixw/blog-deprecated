a987e25f549976af016dbeffca3db83971cba6a5
caixw <xnotepad@gmail.com>
2017-08-23 01:12:38 +0800

    改时 app.js

--- themes/default/app.js
+++ themes/default/app.js
@@ -1,14 +1,27 @@
 "use strict";
 
+/**
+ * 以下是一些公用常量的定义，方便修改配置。
+ */
+const gototopTimes = 20
+const gototopDistance = 30
+const prismAutoloaderPath = '//cdn.bootcss.com/prism/1.6.0/components/'
+
+/**
+ * 程序的入口代码。
+ * DOM 加载完成之后，才执行操作，相当于原来的 jQuery.ready
+ */
 window.document.addEventListener('DOMContentLoaded', ()=>{
     // 语法高亮
-    Prism.plugins.autoloader.languages_path='//cdn.bootcss.com/prism/1.6.0/components/';
-    Prism.highlightAll(false);
+    Prism.plugins.autoloader.languages_path = prismAutoloaderPath
+    Prism.highlightAll(false)
 
     initTop()
 })
 
-// 初始化所有与 goto top 相关的内容
+/**
+ * 初始化所有与 goto top 相关的内容
+ */
 function initTop () {
     const top = window.document.getElementById('top')
 
@@ -18,7 +31,7 @@ function initTop () {
 
     // 根据与页面顶部的距离，控制是否显示 top 按钮。
     const showTopButton = function() {
-        const dsp = scrollTop() > 30 ? 'block' : 'none'
+        const dsp = scrollTop() > gototopDistance ? 'block' : 'none'
         if (dsp !== top.style.display) {
             top.style.display = dsp
         }
@@ -29,16 +42,15 @@ function initTop () {
 
     // 滚动到顶部
     top.addEventListener('click', (event)=>{
-        const times = 20;
         let height = scrollTop()
-        const offset = height / times;
+        const offset = height / gototopTimes
 
         const tick = window.setInterval(function(){
             height -= offset;
-            window.scrollTo(0, height);
+            window.scrollTo(0, height)
 
             if (height <= 0){
-                window.clearInterval(tick);
+                window.clearInterval(tick)
             }
         }, 10);
 
